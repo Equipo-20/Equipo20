9a8471752738f97b92b2bc4e099259e725bec30a
Antonio-92-cpu <antoniooyuelasanchez@gmail.com>
2023-07-20 12:42:59 +0200

    Cambios5

--- src/Components/Pop_up/Pop.jsx
+++ src/Components/Pop_up/Pop.jsx
@@ -25,3 +25,45 @@
 //       setIsOpen(false);
 //     };
 // return (
+//     <div className="contenedor">
+//     {characters.map((character) => (
+//       <div className="texto" key={character.name}>
+//         <img
+//           className="img"
+//           src={character.image}
+//           alt={character.name}
+//           onClick={() => openPopup(character)}
+//         />
+//         <h3 className="h3">{character.name}</h3>
+//         <p>Estado: {character.alive ? "Vivo" : "Muerto"}</p>
+//         <p>Casa: {character.house}</p>
+//       </div>
+//       </>
+//     ))}
+//     <Modal
+//       isOpen={isOpen}
+//       onRequestClose={closePopup}
+//       contentLabel="Información del personaje"
+//     >
+//       {selectedCharacter && (
+//         <div className="popup">
+//           <h1>{selectedCharacter.name}</h1>
+//          <img className="img"
+//           src={selectedCharacter.image}
+//           alt={selectedCharacter.name} />
+//           <p>Color de ojos: {selectedCharacter.eyeColour}</p>
+//           <p>Color del cabello: {selectedCharacter.hairColor}</p>
+//           <p>Estado: {selectedCharacter.alive ? "Vivo" : "Muerto"}</p>
+//           <p>Casa: {selectedCharacter.house}</p>
+//           <p>Especie: {selectedCharacter.species}</p>
+//           <p>Patronus: {selectedCharacter.patronus}</p>
+//           <p>Nacimiento: {selectedCharacter.dateOfBirth}</p>
+//           <p>{selectedCharacter.description}</p>
+//         </div>
+//       )}
+//     </Modal>
+//   </div>
+// );
+// };
+
+// export default Characters;
