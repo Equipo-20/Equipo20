8e3a963a890ef62b5e86cb9c65a447073c16dee9
Antonio-92-cpu <antoniooyuelasanchez@gmail.com>
2023-07-20 11:07:21 +0200

    Cambios3

--- src/Components/Pop_up/Pop.jsx
+++ src/Components/Pop_up/Pop.jsx
@@ -1,28 +1,26 @@
-import { useState, useEffect } from "react";
-import Modal from "react-modal";
-import "./pop.css"
+// import { useState, useEffect } from "react";
+// /*import Modal from "react-modal";*/
+// import "./pop.css"
 
-const Characters = () => {
-    const [characters, setCharacters] = useState([]);
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedCharacter, setSelectedCharacter] = useState(null);
+// const Characters = () => {
+//     const [characters, setCharacters] = useState([]);
+//     const [isOpen, setIsOpen] = useState(false);
+//     const [selectedCharacter, setSelectedCharacter] = useState(null);
   
-    useEffect(() => {
-      fetch("https://Ejemplo de una bases de datos")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setCharacters(data);
-        });
-    }, []);
+//     useEffect(() => {
+//      /* fetch("https://Ejemplo de una bases de datos")
+//         .then((response) => response.json())
+//         .then((data) => {
+//           console.log(data);
+//           setCharacters(data);
+//         });
+//     }, []);*/
   
-    const openPopup = (character) => {
-      setSelectedCharacter(character);
-      setIsOpen(true);
-    };
+//     const openPopup = (character) => {
+//       setSelectedCharacter(character);
+//       setIsOpen(true);
+//     };
   
-    const closePopup = () => {
-      setIsOpen(false);
-    };
-  
-    return ()
+//     const closePopup = () => {
+//       setIsOpen(false);
+//     };
