85e3aad054af6f8b62c41aecde7234064695ba8a
florienborg <flornbg014@gmail.com>
2023-07-20 14:10:45 +0200

    main body de pagina 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Body_pag2 from './Body_pag2/Body2';
 
 function App() {
   return (
-    <div className="App">  
+    <div className="App"> 
+    <Body_pag2 />
     </div>
   );
 }
