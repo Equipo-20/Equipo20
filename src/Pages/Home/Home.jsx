88d603621c77d1029288899fcdcdf2eea4603973
florienborg <flornbg014@gmail.com>
2023-07-20 20:21:53 +0200

    Texto info, home, y boton

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Button} from '../../Components/Button/Button';
 import './Home.css';
 import Footer from '../../Components/Footer/Footer';
+import TextBox from '../../Components/Info_text_box/Text';
 
 function Pag1() {
     return (
     <div className="Pag1">
-        <Button/>
+        <TextBox />
+        <Button />
         <Footer />
     </div>
     )
