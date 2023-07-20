60796c67e3a50533f007d2ad78d8adea492835c7
florienborg <flornbg014@gmail.com>
2023-07-20 21:09:55 +0200

    banner!

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Button} from '../../Components/Button/Button';
 import './Home.css';
+import Banner from '../../Components/Banner/Banner';
 import Footer from '../../Components/Footer/Footer';
 import TextBox from '../../Components/Info_text_box/Text';
 
@@ -8,6 +9,7 @@ function Pag1() {
     return (
     <div className="Pag1">
         <TextBox />
+        <Banner />
         <Button />
         <Footer />
     </div>
